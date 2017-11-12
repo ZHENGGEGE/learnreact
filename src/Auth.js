d18ffc61b97a3094f579c77f27da82ced8db08f7
ZHENGGEGE <593012736@qq.com>
2017-11-12 18:09:27 +0800

    前后联调

--- src/Auth.js
+++ src/Auth.js
@@ -1,19 +1,43 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import {login} from './Auth.redux'
+import {login,getUserData} from './Auth.redux'
 import {Redirect} from 'react-router-dom'
+// import axios from 'axios'
 
 //两个reducers 每个reducer都有一个state
 //合并reducer
 @connect(
     state => state.auth,
-    {login}
+    {login,getUserData}
 )
 
 class Auth extends React.Component{
+
+    /* 获取服务端数据 不和redux配合的情况*/
+    // constructor(props){
+    //     super(props)
+    //     this.state = {
+    //         data : {}
+    //     }
+    // }
+    // componentDidMount(){
+    //     axios.get('/data').then(res => {
+    //         if(res.status === 200){
+    //             this.setState({data:res.data})
+    //             console.log(res)
+    //         }
+            
+    //     })
+    // }
+
+    componentDidMount(){
+        this.props.getUserData()
+    }
    render(){
        return (   
            <div>
+               {/* <h2>我的名字是{this.state.data.user},年龄{this.state.data.age}</h2> */}
+               <h2>我的名字是{this.props.user},年龄{this.props.age}</h2>
                { this.props.isAuth ? <Redirect to="/dashbord" /> : null} 
                 <h1>你没有权限，需要登录才能看</h1>
                 <button onClick={this.props.login}>登录</button>
--- src/Auth.redux.js
+++ src/Auth.redux.js
@@ -1,19 +1,46 @@
+import axios from 'axios'
 const LONGIN = 'LONGIN'
 const LOGOUT = 'LOGOUT'
+const USER_DATA = 'USER_DATA'
 
-
+const initState = {
+    isAuth:false,
+    user:'李云龙',
+    age:20
+}
 //reducer
-export function auth(state={isAuth:false,user:'李云龙'},action){
+export function auth(state=initState,action){
+    console.log(state,action)
     switch(action.type){
         case LONGIN :
             return {...state,isAuth : true} 
         case LOGOUT :
             return {...state,isAuth : false}
+        case USER_DATA : 
+            return {...state,user:action.payload.user,age:action.payload.age}
         default : 
             return state
     }
 }
 
+export function getUserData(){
+    //dispatch用来通知数据修改
+    return dispatch => {
+             axios.get('/data').then(res => {
+            if(res.status === 200){
+                dispatch(UserData(res.data))
+                console.log(res)
+            }
+            
+        })
+    }
+}
+export function UserData(data){
+    return {
+        type:USER_DATA,
+        payload:data
+    }
+}
 //action creator
 export function login(){
     return {type:LONGIN}
